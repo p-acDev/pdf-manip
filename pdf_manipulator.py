770be354b4f08ba548b3135e1f0044e13f15b6c4
pacourbet <pierreandre.courbet@gmail.com>
2022-04-05 14:39:55 +0200

    clean the code

--- pdf_manipulator.py
+++ pdf_manipulator.py
@@ -1,23 +1,9 @@
 import sys
-from PyQt5 import QtWidgets, QtGui, QtCore
-from gui_pdf_manipulator import Ui_MainWindow
-from gui_removePages import Ui_RemovePagesWindow
 from time import sleep
 import os
 import sys
 import version
 from urllib.parse import quote
-
-
-def resource_path(relative_path):
-    """ Get the absolute path to the resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
 
 def merge(pdfs):
     
